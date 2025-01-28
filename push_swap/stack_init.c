8aca41ab9b4a5b6a70638199b3cdd6d79e07407f
Augusto César <augustocs.ita@gmail.com>
2025-01-27 21:23:00 -0300

    news_functions

--- push_swap/stack_init.c
+++ push_swap/stack_init.c
@@ -10,7 +10,54 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
+
+static long ft_atol(const char *s)
+{
+	long	result;
+	int		sign;
+
+	result = 0;
+	sing = 1;
+	while (*s == '' || *s >= 9 && s <= 13)
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (ft_isdigit(*s))
+		result = result * 10 + (*str++ - '0');
+	return (result * sign);
+}
+
+static void	append_node(t_stack_node **stack, int n)
+{
+	t_stack_node	*node;
+	t_stack_node	*last_node;
+
+	if (!stack)
+		return ;
+	node = malloc(sizeof(t_stack_node));
+	if (!node)
+		return ;
+	node->next = NULL;
+	node->value	= n;
+	node->cheapest = 0;
+
+	if (!(*stack))
+	{
+		*stack = node;
+		node->prev = NULL;
+	}
+	else
+	{
+		last_node = find_last(*stack);
+		last_node->next = node;
+		node->prev = last_node;
+	}
+}
 
 void	init_stack_a(t_stack_node **a, char **argv)
 {
@@ -30,4 +77,36 @@ void	init_stack_a(t_stack_node **a, char **argv)
 	}
 }
 
-t
+t_stack_node	*get_cheapest(t_stack_node *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack)
+	{
+		if (stack->cheapest)
+			return (stack);
+		stack = stack->next;
+	}
+	return (NULL);
+}
+
+void prep_for_push(t_stack_node **stack, t_stack_node *top_node, char stack_name)
+{
+	while (*stack != top_node)
+	{
+		if (stack_name = 'a')
+		{
+			if (top_node->above_median)
+				ra(stack, false);
+			else
+				rra(stack, false);
+		}
+		else if (stack_name = 'b')
+		{
+			if (top_node->above_median)
+				rb(stack, false);
+			else
+				rrb(stack, false);
+		}
+	}
+}
